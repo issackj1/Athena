7719eee3628da81ee219aa03512b732bc8bfac0c
issackj1 <issack.john1@ucalgary.ca>
2021-05-13 01:36:02 -0600

    removed unnecessary code

--- athena-ui/src/App.tsx
+++ athena-ui/src/App.tsx
@@ -5,43 +5,28 @@ import { PrivateRoute } from "./components/PrivateRoute";
 import { TableDetail } from "./components/TableDetail";
 import { Endpoint } from "./components/Endpoint";
 import { Welcome } from "./components/Welcome";
-import { Box, Container, Grid, makeStyles } from "@material-ui/core";
+import { Box } from "@material-ui/core";
 import { AthenaNavBar } from "./components/AthenaNavBar";
 import { Copyright } from "./components/Copyright";
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-}));
-
 export default function App() {
-  const classes = useStyles();
-
   return (
-    <Container className={classes.paper}>
-      <Grid>
-        <Router basename={process.env.PUBLIC_URL}>
-          <Grid item>
-            <AthenaNavBar />
-          </Grid>
-          <Switch>
-            <Route path={"/auth"} component={Auth} />
-            <PrivateRoute
-              path={"/:name/detail/:productId"}
-              component={TableDetail}
-            />
-            <PrivateRoute path={"/athena/:name"} component={Endpoint} />
-            <PrivateRoute path={"/"} component={Welcome} />
-          </Switch>
-        </Router>
-      </Grid>
+    <>
+      <Router basename={process.env.PUBLIC_URL}>
+        <AthenaNavBar />
+        <Switch>
+          <Route path={"/auth"} component={Auth} />
+          <PrivateRoute
+            path={"/:name/detail/:productId"}
+            component={TableDetail}
+          />
+          <PrivateRoute path={"/athena/:name"} component={Endpoint} />
+          <PrivateRoute path={"/"} component={Welcome} />
+        </Switch>
+      </Router>
       <Box mt={8}>
         <Copyright />
       </Box>
-    </Container>
+    </>
   );
 }
--- athena-ui/src/components/Welcome.tsx
+++ athena-ui/src/components/Welcome.tsx
@@ -7,18 +7,16 @@ interface Props {}
 
 export const Welcome: React.FC<Props> = (props) => {
   return (
-    <Container maxWidth="sm">
-      <Box my={20}>
-        <Typography
-          color={"textPrimary"}
-          component={"h5"}
-          variant={"h2"}
-          align={"center"}
-        >
-          What would you like to do?
-        </Typography>
-        <Home />
-      </Box>
-    </Container>
+    <Box my={20}>
+      <Typography
+        color={"textPrimary"}
+        component={"h5"}
+        variant={"h2"}
+        align={"center"}
+      >
+        What would you like to do?
+      </Typography>
+      <Home />
+    </Box>
   );
 };
